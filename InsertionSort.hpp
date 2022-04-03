f31cab19b132a662eb11d2dcfda28f931405f237
letsexisttogether <71719320+letsexisttogether@users.noreply.github.com>
2022-04-03 22:49:01 +0300

    Update InsertionSort.hpp

--- InsertionSort.hpp
+++ InsertionSort.hpp
@@ -4,10 +4,10 @@
 template <class T>
 void InsertionSort(std::vector<T>& sVec)
 {
-    for (size_t i = 1; i < sVec.size(); i++)
+    for (int i = 1; i < sVec.size(); i++)
     {
         T key = sVec[i];
-        size_t j = i - 1;
+        int j = i - 1;
         while (j >= 0 && sVec[j] > key)
         {
             sVec[j + 1] = sVec[j];
